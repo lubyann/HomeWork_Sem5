caf5634c810afb1158e9fbe268d4b3854ad4d8b7
Maksim Lubyannikov <m.lubyann@gmail.com>
2023-02-26 20:24:07 +0300

    add task34

---
+++ Task_034/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int num = 6;
+int[] array = GetBinaryArray(num);
+//Console.WriteLine($"[{String.Join(", ", array)}] -> {EvenNumbers}");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(99, 1000);
+    }
+    return result;
+}
+
+int res = 0;
+int EvenNumbers(int res)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] % 2 == 0)
+            res++;
+
+    }
+    return res;
+}
+Console.WriteLine($"[{String.Join(", ", array)}] -> {EvenNumbers(res)}");
+
+//Console.WriteLine(EvenNumbers(res));
